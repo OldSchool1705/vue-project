a539698509aede3e7b1440a8112160a1ec5b0a05
Artem <oldschool1705@gmail.com>
2021-11-22 21:05:29 +0300

    add posts button dont click

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,82 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <form>
+      <h4>создание поста</h4>
+      <input
+        v-bind:value="title"
+        @input="title = $event.target.value"
+        class="input"
+        type="text"
+        placeholder="Название"
+      />
+      <input
+        v-bind:value="body"
+        @input="body = $event.target.value"
+        class="input"
+        type="text"
+        placeholder="Описание"
+      />
+      <button class="btn" @click="createPost">Создать</button>
+    </form>
+    <div class="post" v-for="post in posts" :key="post">
+      <div><strong>Название</strong> {{ post.title }}</div>
+      <div><strong>Описание</strong> {{ post.body }}</div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      posts: [
+        { id: 1, title: "javaScript", body: "Описание" },
+        { id: 2, title: "javaScript 2", body: "Описание 2" },
+        { id: 3, title: "javaScript 3", body: "Описание 3" },
+      ],
+      title: "",
+      body: "",
+    };
+  },
+  methods: {
+    createPost: {},
+    
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
+}
+
+.input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+  outline: none;
+}
+form {
+  display: flex;
+  flex-direction: column;
+}
+.btn {
+  margin: 15px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+}
+.app {
+  padding: 20px;
 }
 </style>
